882dab32ca1bbfe4cf2b9ebdeb4cf67f741f82c0
unknown <dima>
2025-09-27 19:39:31 +0300

    components learn

---
+++ nuxt-app/app/components/post/Item.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-white w-full p-4 border border-gray-200 mb-4">
+        <h3 class="mb-2 text-lg text-gray-700">Post title 1</h3>
+        <p class="text-xs text-gray-500">Post content</p>
+    </div>  
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+
+</style>
